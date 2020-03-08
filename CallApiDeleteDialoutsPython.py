42f5c0c11a8a443aab6f62064e6f39b8a55c0101
danilfg <danilfg@yahoo.com>
2020-03-08 11:54:20 +0300

    fix

--- CallApiDeleteDialoutsPython.py
+++ CallApiDeleteDialoutsPython.py
@@ -1,11 +1,13 @@
 import requests
 
+
 def RunDeleteDialOuts():
     responseDelete = DeleteDialOuts()
     if responseDelete['status'] == 'error':
         if responseDelete['errorCode'] == 10011:
             CompleteDialOuts()
             DeleteDialOuts()
+
 
 def DeleteDialOuts():
     allDialOuts = requests.post(urlList, headers=headers, json={'token': tokenEnter}).json()
@@ -15,16 +17,19 @@ def DeleteDialOuts():
         print ("Delete ID:", allDialOuts['result'][0]['id'])
     return (responseDelete)
 
+
 def CompleteDialOuts():
     listForCompleteDial = requests.post(urlList, headers=headers, json={'token': tokenEnter}).json()
     jsonCompleteDial = {'token': tokenEnter, 'dialoutid': listForCompleteDial['result'][0]['id']}
     requests.post(urlComplete, headers=headers, json=jsonCompleteDial)
+
 
 def CheckToken():
     forCheckToken = requests.post(urlList, headers=headers, json={'token': tokenEnter}).json()
     if forCheckToken['status'] == 'error':
         print("May be invalid token!")
         exit()
+
 
 urlDel = 'https://callapi.gravitel.ru/api/v1/deletedialout'
 urlList = 'https://callapi.gravitel.ru/api/v1/listdialouts'
